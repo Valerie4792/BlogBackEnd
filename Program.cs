96fa545357e55666e4b6f323c1fa639fd6b8900e
Valerie4792 <vaguilar4792@gmail.com>
2023-10-17 20:15:47 -0700

    updatd link in program cs

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(conne
 builder.Services.AddCors(options => {
     options.AddPolicy("BlogPolicy", 
     builder => {
-        builder.WithOrigins("http://localhost:5174") //the local host is what we grab from the webpage link
+        builder.WithOrigins("http://localhost:5173") //the local host is what we grab from the webpage link
         .AllowAnyHeader()
         .AllowAnyMethod();
     });
